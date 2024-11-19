f389b06616f7fdbaaff6b42acc49c84355cb71d2
Jhoan <jhoangongora71@gmail.com>
2024-11-18 23:49:40 -0500

    Cambio configuracion railway

--- database/db.js
+++ database/db.js
@@ -1,16 +1,22 @@
-const mysql = require('mysql'); //importando el modulo de base de datos
+require('dotenv').config(); // Carga las variables de entorno desde el archivo .env
+const mysql = require('mysql'); // Importando el módulo de base de datos
+
+// Configuración de la conexión
 const conexion = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'crud_nodes_db'  
+    host     : process.env.MYSQLHOST || 'localhost', // Usará la variable de entorno o 'localhost' como predeterminado
+    user     : process.env.MYSQLUSER || 'root',      // Usará la variable de entorno o 'root' como predeterminado
+    password : process.env.MYSQLPASSWORD || '',      // Usará la variable de entorno o contraseña vacía como predeterminado
+    database : process.env.MYSQLDATABASE || 'crud_nodes_db', // Usará la variable de entorno o tu base de datos local
+    port     : process.env.MYSQLPORT || 3306         // Usará la variable de entorno o el puerto 3306
 });
 
-conexion.connect((error)=>{
+// Conexión a la base de datos
+conexion.connect((error) => {
     if (error) {
-      console.error('El error de conexión es: ' + error);
-      return;
+        console.error('El error de conexión es: ' + error);
+        return;
     }
     console.log('¡Conectado a la Base de Datos!');
-  });
+});
+
 module.exports = conexion;
