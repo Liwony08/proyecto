72595d9c00401015f026a081b6c90718735c506c
Liwony08 <lizz.aleja08@gmail.com>
2024-11-20 20:30:27 -0500

    Cambio

--- database/db.js
+++ database/db.js
@@ -1,22 +1,22 @@
-require('dotenv').config(); // Carga las variables de entorno desde el archivo .env
-const mysql = require('mysql'); // Importando el módulo de base de datos
+require('dotenv').config(); // Cargar variables de entorno desde .env
+const mysql = require('mysql2'); // Importando el módulo de base de datos
 
 // Configuración de la conexión
 const conexion = mysql.createConnection({
-    host     : process.env.MYSQLHOST || 'localhost', // Usará la variable de entorno o 'localhost' como predeterminado
-    user     : process.env.MYSQLUSER || 'root',      // Usará la variable de entorno o 'root' como predeterminado
-    password : process.env.MYSQLPASSWORD || '',      // Usará la variable de entorno o contraseña vacía como predeterminado
-    database : process.env.MYSQLDATABASE || 'crud_nodes_db', // Usará la variable de entorno o tu base de datos local
-    port     : process.env.MYSQLPORT || 3306         // Usará la variable de entorno o el puerto 3306
+    host: process.env.MYSQLHOST || 'localhost',
+    user: process.env.MYSQLUSER || 'root',
+    password: process.env.MYSQLPASSWORD || '',
+    database: process.env.MYSQLDATABASE || 'crud_nodes_db',
+    port: process.env.MYSQLPORT || 3306
 });
 
 // Conexión a la base de datos
 conexion.connect((error) => {
     if (error) {
-        console.error('El error de conexión es: ' + error);
+        console.error('El error de conexión es: ' + error.message);
         return;
     }
-    console.log('¡Conectado a la Base de Datos!');
+    console.log('Conectado a la Base de Datos');
 });
 
 module.exports = conexion;
